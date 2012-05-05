41b6c39dbb1e08fef6b1941383f16204d61ef31e
Rosario Raulin <raulin@st.ovgu.de>
2012-05-05 15:50:47 +0200

    add incidentially removed test cases

---
+++ test/graph/IPathFinderTest.java
@@ -0,0 +1,43 @@
+package graph;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class IPathFinderTest {
+
+	@Test
+	public void test() {
+		IGraph<String> graph = new AdjacencyList<String>();
+		graph.addNode("A");
+		graph.addNode("B");
+		graph.addNode("C");
+		graph.addNode("D");
+		graph.addNode("E");
+		graph.addNode("F");
+		
+		graph.addEdge("A", "B", 10);
+		graph.addEdge("A", "C", 10);
+		
+		graph.addEdge("B", "E", 10);
+		graph.addEdge("E", "F", 3);
+		
+		graph.addEdge("C", "D", 5);
+		graph.addEdge("D", "F", 12);
+		
+		// IPathFinder<String> pathfinder = new DijkstraAlgorithm<String>(graph);
+		IPathFinder<String> pathfinder = new AStarAlgorithm<String>(graph);
+		
+		try {
+			Stack<String> path = pathfinder.findPath("A", "F");
+			assertTrue(path.pop().equals("B"));
+			assertTrue(path.pop().equals("E"));
+			assertTrue(path.pop().equals("F"));
+		} catch (NoPathFoundException e) {
+			fail(e.toString());
+		}
+	}
+
+}
